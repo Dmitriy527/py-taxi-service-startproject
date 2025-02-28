50cec867b657e1b29a14f02aab4ea9d79d3a0c3d
Dmytro <dimkanividimka4@gmail.com>
2025-02-28 13:14:42 +0200

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -10,7 +10,7 @@ class Manufacturer(models.Model):
 
 
 class Driver(AbstractUser):
-    license_number = models.CharField(max_length=100)
+    license_number = models.CharField(max_length=100, unique=True)
 
 
 class Car(models.Model):
